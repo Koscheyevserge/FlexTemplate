e90b08517ab93f748371349cb0a8c43010a10f7b
Tychin_Vlad <simpleghost69@gmail.com>
2017-01-30 13:00:40 +0200

    [ADD] Создание ViewComponent ThisCityPlace
    Создание ViewComponent ThisCityPlace и класса ThisCityPlaceViewModel

---
+++ src/FlexTemplate/ViewModels/ThisCityPlaceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlexTemplate.ViewModels
+{
+    public class ThisCityPlaceViewModel
+    {
+        public string PhotoPath { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public float Stars { get; set; }
+        public int ReviewsCount { get; set; }
+        public virtual List<ThisCityPlaceViewModel> Categories { get; set; }
+
+
+
+      
+    }
+}
