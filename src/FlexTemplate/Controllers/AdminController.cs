256fada209360b262e28b23e3daf295e4a4a74d8
Koscheyevserge <koscheyevserge4@yandex.ru>
2017-02-12 19:30:12 +0200

    [ADD] Сделан тестовый апи для категорий
    GET http://localhost:27367/api/category/1

--- src/FlexTemplate/Controllers/AdminController.cs
+++ src/FlexTemplate/Controllers/AdminController.cs
@@ -6,24 +6,34 @@ using FlexTemplate.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FlexTemplate.Database;
+using FlexTemplate.Entities;
+using FlexTemplate.ViewComponents;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace FlexTemplate.Controllers
 {
     public class AdminController : Controller
     {
         private readonly Context db;
+        private readonly UserManager<User> um;
+        private readonly RoleManager<IdentityRole> rm; 
 
-        public AdminController(Context context)
+        public AdminController(Context context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             db = context;
+            rm = roleManager;
+            um = userManager;
         }
 
         #region UserRoles
 
         [HttpGet]
-        public IActionResult UserRoles()
+        [Route("/Admin/UserRoles/{page}")]
+        public IActionResult UserRoles(int page)
         {
-            var model = db.UserRoles.Take(10);
+            page--;
+            var model = db.Roles.Skip(10 * page).Take(10).AsNoTracking();
             return View(model);
         }
         #endregion
@@ -31,9 +41,11 @@ namespace FlexTemplate.Controllers
         #region Users
 
         [HttpGet]
-        public IActionResult Users()
+        [Route("/Admin/Users/{page}")]
+        public IActionResult Users(int page)
         {
-            var model = db.Users.Take(10);
+            page--;
+            var model = db.Users.Skip(10 * page).Take(10).AsNoTracking();
             return View(model);
         }
         #endregion
@@ -41,9 +53,11 @@ namespace FlexTemplate.Controllers
         #region Categories
 
         [HttpGet]
-        public IActionResult Categories()
+        [Route("/Admin/Categories/{page}")]
+        public IActionResult Categories(int page)
         {
-            var model = db.Categories.Take(10).Include(ur => ur.Aliases);
+            page--;
+            var model = db.Categories.Skip(10 * page).Take(10).AsNoTracking();
             return View(model);
         }
         #endregion
@@ -51,31 +65,145 @@ namespace FlexTemplate.Controllers
         #region UserRole
 
         [HttpGet]
-        public IActionResult UserRole(int id)
+        [Route("/Admin/UserRole/{id}")]
+        public IActionResult UserRole(string id)
         {
-            var model = db.UserRoles.FirstOrDefault();
+            var model = db.Roles.FirstOrDefault(i => i.Id == id);
             return View(model);
         }
         #endregion
 
-
         #region User
 
         [HttpGet]
-        public new IActionResult User(int id)
+        [Route("/Admin/User/{id}")]
+        public new IActionResult User(string id)
         {
-            var model = db.Users.FirstOrDefault();
+            var model = db.Users.FirstOrDefault(i => i.Id == id);
             return View(model);
         }
+
+        [HttpPost]
+        [Route("api/user/create")]
+        public async Task<AjaxResponse> CreateUser([FromBody]User item)
+        {
+            var result = await um.CreateAsync(item);
+            return new AjaxResponse
+            {
+                ErrorMessages = result.Errors.Select(error => error.Description),
+                Successed = result.Succeeded
+            };
+        }
+
         #endregion
 
         #region Category
 
         [HttpGet]
+        [Route("/Admin/{id}")]
         public IActionResult Category(int id)
         {
-            var model = db.Categories.FirstOrDefault();
+            var model = GetCategory(id);
             return View(model);
+        }
+
+        [HttpGet]
+        [Route("api/category/{id}")]
+        public Category GetCategory(int id)
+        {
+            return db.Categories.FirstOrDefault(i => i.Id == id);
+        }
+
+        [HttpPost]
+        [Route("api/category/create")]
+        public AjaxResponse CreateCategory([FromBody]Category item)
+        {
+            try
+            {
+                item.Id = 0;
+                db.Categories.Add(item);
+                db.SaveChanges();
+                if (item.Id != 0)
+                {
+                    return new AjaxResponse
+                    {
+                        Successed = true
+                    };
+                }
+                return new AjaxResponse
+                {
+                    Successed = false
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AjaxResponse
+                {
+                    ErrorMessages = new List<string> { ex.Message },
+                    Successed = false
+                };
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/category/delete")]
+        public AjaxResponse DeleteCategory(int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    db.Categories.FirstOrDefault(i => i.Id == id);
+                    return new AjaxResponse
+                    {
+                        Successed = true
+                    };
+                }
+                return new AjaxResponse
+                {
+                    Successed = false
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AjaxResponse
+                {
+                    ErrorMessages = new List<string> { ex.Message },
+                    Successed = false
+                };
+            }
+
+        }
+
+        [HttpPut]
+        [Route("api/category/update")]
+        public AjaxResponse UpdateCategory([FromBody]Category item)
+        {
+            try
+            {
+                if (item.Id != 0)
+                {
+                    item.Id = 0;
+                    db.Categories.Update(item);
+                    db.SaveChanges();
+                    return new AjaxResponse
+                    {
+                        Successed = true
+                    };
+                }
+                return new AjaxResponse
+                {
+                    Successed = false
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AjaxResponse
+                {
+                    ErrorMessages = new List<string> { ex.Message },
+                    Successed = false
+                };
+            }
         }
         #endregion
     }
---
+++ src/FlexTemplate/ViewComponents/AjaxResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlexTemplate.ViewComponents
+{
+    public class AjaxResponse
+    {
+        public bool Successed { get; set; }
+        public IEnumerable<string> ErrorMessages { get; set; }
+    }
+}
