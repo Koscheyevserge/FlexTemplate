ba59fe0bc2d2211054ebcc44ada1a58e9f0d452e
Timofey <savtym@gmail.com>
2017-03-04 17:57:22 +0200

    [ADD] pages for admin

--- src/FlexTemplate/wwwroot/js/admin.js
+++ src/FlexTemplate/wwwroot/js/admin.js
@@ -1,13 +1,21 @@
 "use strict"
 
+//category
 const POST_UPDATE_CATEGORY = "/api/category/update";
 const GET_CREATE_CATEGORY = "/api/category/create"; 
 const GET_REMOVE_CATEGORY = "/api/category/delete/";
 
 const GET_CREATE_ALIAS = "/api/category/createalias/";
 
+
+const POST_UPDATE_PAGES = '/api/page/update';
+const GET_CREATE_PAGES = '/api/page/create';
+const GET_REMOVE_PAGES = '/api/page/remove/';
+
 /* url */
 const URL_DOMAIN = window.location.origin;
+
+
 
 /* create alias */
 $('.c-categories').on('click', '.admin-button-add-alias', function() {
@@ -23,13 +31,10 @@ $('.c-categories').on('click', '.admin-button-add-alias', function() {
 	});
 });
 
-
 /* remove alias */
 $('.c-categories').on('click','.admin-button-alias-remove', function() {
 	$(this).parent().remove();
 });
-
-
 
 /* create category */
 $('.c-categories').on('click', '.admin-button-add-category', function() {
@@ -42,7 +47,6 @@ $('.c-categories').on('click', '.admin-button-add-category', function() {
     }
   });
 });
-
 
 /* remove category */
 $('.c-categories').on('click', '.admin-button-remove-category', function() {
@@ -60,7 +64,7 @@ $('.c-categories').on('click', '.admin-button-remove-category', function() {
     	console.log("Status: " + data.successed + "\nMessage: " + data.errorMessages);
     }
 	});
-});		    			 		
+});
 
 /* update category */
 $('.c-categories').on('click', '.admin-button-category-save', function() {
@@ -94,16 +98,44 @@ $('.c-categories').on('click', '.admin-button-category-save', function() {
 });
 
 
-// var panelList = $('#draggablePanelList');
+//pages
 
-// panelList.sortable({
-//   // Only make the .panel-heading child elements support dragging.
-//   // Omit this to make then entire <li>...</li> draggable.
-//   handle: '.panel-heading', 
-//   update: function() {
-//     $('.panel', panelList).each(function(index, elem) {
-// 			var $listItem = $(elem),
-// 			   newIndex = $listItem.index();
-//     });
-//   }
-// });
+/* update pages */
+$('.c-pages').on('click', '.admin-button-pages-save', function() {
+	let page = $(this).parents(".page");
+	let componentsParent = page.find(".component");
+	let components = [];
+	for (let i = 0; i < componentsParent.length; i++) {
+    components.push({
+			ContainerTemplateId: +$(componentsParent[i]).attr('dataId'),
+			Position: i
+		});
+	};
+	let obj = {
+		
+			id: +page.attr("dataId"),
+			Name: page.find("#pageName").val(),
+			BodyClasses: page.find("#pageNameClass").val(),
+			Title: page.find("#pageNameTitle").val(),
+			PageContainerTemplates: components
+	}
+	$.ajax({
+	  url: URL_DOMAIN + POST_UPDATE_CATEGORY,
+    datatype: 'json',
+    type: "post", 
+    contentType: "application/json",
+	  data: JSON.stringify({
+			id: +page.attr("dataId"),
+			Name: page.find("#pageName").val(),
+			BodyClasses: page.find("#pageNameClass").val(),
+			Title: page.find("#pageNameTitle").val(),
+			PageContainerTemplates: components
+	  }),
+		success: function(data) {
+    	if (!data.successed) {
+    		alert('Error: ' + data.errorMessages);
+    	}
+			console.log("Status: " + data.successed + "\nMessage: " + data.errorMessages);
+		}
+	});
+});
