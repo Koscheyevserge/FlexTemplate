96c11b678a640045b839df48adb9f99ace41ccc5
Tychin-V <simpleghost69@gmail.com>
2017-03-20 21:49:14 +0200

    [ADD] Добавление новых компонентов

---
+++ src/FlexTemplate/ViewComponents/HomeController/ThisPlaceLocation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlexTemplate.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexTemplate.ViewComponents.HomeController
+{
+    public class ThisPlaceLocation : ViewComponent
+    {
+        private readonly Context _context;
+
+        public ThisPlaceLocation(Context context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
