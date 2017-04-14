b2b81e841a35ef97bd0bcc0a87a544071248d998
Tychin-V <simpleghost69@gmail.com>
2017-04-14 21:33:19 +0300

    [FIX] Вьюха Blog и компонент BlogComments

---
+++ src/FlexTemplate/ViewComponents/HomeController/BlogComments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlexTemplate.Database;
+using Microsoft.AspNetCore.Mvc;
+using FlexTemplate.Entities;
+using FlexTemplate.ViewModels.HomeController;
+
+
+namespace FlexTemplate.ViewComponents.HomeController
+{
+    public class BlogComments : ViewComponent
+    {
+        private readonly Context _context;
+
+        public BlogComments(Context context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(BlogComment item)
+        {
+            return View(new HomeBlogCommentsViewModel { Comment = item });
+        }
+    }
+}
