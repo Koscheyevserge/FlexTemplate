9e132865f17d089ed76dd1a4017bff7e7064fe67
Koscheyevserge <koscheyevserge4@yandex.ru>
2017-04-30 11:53:08 +0300

    [ADD] Валидация текстовых полей с классом "numeric"

--- src/FlexTemplate/wwwroot/js/customs.js
+++ src/FlexTemplate/wwwroot/js/customs.js
@@ -555,6 +555,8 @@ function initSlider() {
           $(this).prop('required', true);
       });
   });
+  //Поля в которые можно вводить только числа и знаки препинания
+  $('body').on('keydown', '.numeric', function (e) { -1 !== $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 188]) || /65|67|86|88/.test(e.keyCode) && (!0 === e.ctrlKey || !0 === e.metaKey) || 35 <= e.keyCode && 40 >= e.keyCode || (e.shiftKey || 48 > e.keyCode || 57 < e.keyCode) && (96 > e.keyCode || 105 < e.keyCode) && e.preventDefault() });
 
   /*
   * CKEditor config
