98e10567cecbf2b0cae5223c170c90599f1362a1
Koscheyevserge <koscheyevserge4@yandex.ru>
2017-07-23 12:22:56 +0300

    [ADD] ThumbnailWebJob

--- FlexTemplate.BlobAccessLayer/Services/ImagesServices.cs
+++ FlexTemplate.BlobAccessLayer/Services/ImagesServices.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 
@@ -10,6 +12,7 @@ namespace FlexTemplate.BlobAccessLayer.Services
     public class ImagesServices
     {
         private CloudBlobClient BlobClient { get; set; }
+
         public ImagesServices(StorageAccountOptions options)
         {
             var cloudStorageAccount = CloudStorageAccount.Parse(
@@ -18,6 +21,24 @@ namespace FlexTemplate.BlobAccessLayer.Services
                 $"AccountKey={options.StorageAccountKey};" +
                 $"EndpointSuffix=core.windows.net;");
             BlobClient = cloudStorageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task<List<Uri>> UploadBlob(string blobContainer, IFormFile file, string directoryName)
+        {
+            var container = BlobClient.GetContainerReference(blobContainer);
+            await container.CreateIfNotExistsAsync();
+            var permissions = new BlobContainerPermissions {PublicAccess = BlobContainerPublicAccessType.Blob};
+            await container.SetPermissionsAsync(permissions);
+            BlobContinuationToken continuationToken= null;
+            var blobItems = new List<Uri>();
+            do
+            {
+                var response = await container.ListBlobsSegmentedAsync(continuationToken);
+                continuationToken = response.ContinuationToken;
+                blobItems.AddRange(response.Results.OfType<CloudBlockBlob>().Select(cbb => cbb.Uri));
+            }
+            while (continuationToken != null);
+            return blobItems;
         }
     }
 }
